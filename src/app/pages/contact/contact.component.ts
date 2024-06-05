92316be7d1e3f0d66fe7e9187dce27616042945d
Skiioo <merletti.sacha@gmail.com>
2024-06-05 17:58:15 +0200

    update formulaire 05/06/2024

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../data.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  data: any = {
+    nom: '',
+    prenom: '',
+    email: '',
+    num: '',
+    description: ''
+  };
+
+  constructor(private dataService: DataService) { }
+
+  postData() {
+    this.dataService.postData(this.data).subscribe(
+      response => {
+        console.log('Données envoyées avec succès', response);
+      },
+      error => {
+        console.error('Erreur lors de l\'envoi des données', error);
+      }
+    );
+  }
 }
